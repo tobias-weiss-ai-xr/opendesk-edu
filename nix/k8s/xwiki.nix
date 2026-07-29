4821b33a6a1b75eef33595e7b58d5fc80e412daa
Tobias Weiß <weissto@hrz.uni-marburg.de>
2026-07-29 23:23:57 +0200

    feat(nix): migrate 10 services to use ingressWithCert for automatic TLS
    Updated services to use cert-manager integration:
    - bookstack, drawio, ilias, moodle, openproject
    - planka, sogo, stalwart, xwiki, excalidraw
    
    Each service now generates:
    - Certificate resource (requests TLS from opendesk-ca)
    - Ingress resource (configures HTTPS with auto-generated cert)
    
    Signed-off-by: Hermes Agent

--- nix/k8s/xwiki.nix
+++ nix/k8s/xwiki.nix
@@ -3,5 +3,4 @@ let name = "xwiki"; image = "ghcr.io/opendesk-edu/xwiki"; tag = "latest";
 in
 [ (lib.deployment { inherit name image tag; port = 80; })
   (lib.service { inherit name; port = 80; })
-  (lib.ingress { inherit name; host = "xwiki.opendesk.hrz.uni-marburg.de"; port = 80; })
-]
+] ++ (lib.ingressWithCert { inherit name; host = "xwiki.opendesk.hrz.uni-marburg.de"; port = 80; })
