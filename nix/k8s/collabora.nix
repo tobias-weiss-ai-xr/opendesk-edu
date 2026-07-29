ba0400030962f61d057256203907f54dade42a8c
Tobias Weiß <weissto@hrz.uni-marburg.de>
2026-07-29 23:28:43 +0200

    fix(nix): remove duplicate lib.ingress from 3 services
    Removed leftover lib.ingress definitions from:
    - collabora.nix
    - element.nix
    - ilias-full.nix
    
    These files had both old ingress and new ingressWithCert,
    which would create duplicate Ingress resources.
    
    Signed-off-by: Hermes Agent

--- nix/k8s/collabora.nix
+++ nix/k8s/collabora.nix
@@ -3,5 +3,4 @@ let name = "collabora"; image = "ghcr.io/opendesk-edu/collabora"; tag = "latest"
 in
 [ (lib.deployment { inherit name image tag; port = 80; })
   (lib.service { inherit name; port = 80; })
-  (lib.ingress { inherit name; host = "collabora.opendesk.hrz.uni-marburg.de"; port = 80; })
-   ] ++ (lib.ingressWithCert { inherit name; host = "collabora.opendesk.hrz.uni-marburg.de"; port = 80; })
+] ++ (lib.ingressWithCert { inherit name; host = "collabora.opendesk.hrz.uni-marburg.de"; port = 80; })
--- nix/k8s/element.nix
+++ nix/k8s/element.nix
@@ -3,5 +3,4 @@ let name = "element"; image = "ghcr.io/opendesk-edu/element"; tag = "latest";
 in
 [ (lib.deployment { inherit name image tag; port = 80; })
   (lib.service { inherit name; port = 80; })
-  (lib.ingress { inherit name; host = "element.opendesk.hrz.uni-marburg.de"; port = 80; })
-   ] ++ (lib.ingressWithCert { inherit name; host = "element.opendesk.hrz.uni-marburg.de"; port = 80; })
+] ++ (lib.ingressWithCert { inherit name; host = "element.opendesk.hrz.uni-marburg.de"; port = 80; })
