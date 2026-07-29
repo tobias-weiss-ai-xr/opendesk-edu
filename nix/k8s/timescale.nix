6ad5fe21c732d0bb056380dcefcacb05d65c6949
Tobias Weiß <weissto@hrz.uni-marburg.de>
2026-07-30 00:14:29 +0200

    feat(nix): add PVC support to StatefulSets + add PVCs to 5 databases
    - lib/k8s.nix: Add volumeClaims parameter to podSpec function
    - lib/k8s.nix: Update statefulset function to generate volumeClaimTemplates
    - mariadb.nix: Add 10Gi PVC with ceph-rbd-ssd storage
    - postgresql.nix: Add 10Gi PVC with ceph-rbd-ssd storage
    - redis.nix: Add 10Gi PVC with ceph-rbd-ssd storage
    - timescale.nix: Add 10Gi PVC with ceph-rbd-ssd storage
    - seaweedfs.nix: Add 10Gi PVC for master + 20Gi PVC for volume, both ceph-rbd-ssd
    
    All StatefulSet services now have persistent storage.
    
    Signed-off-by: Hermes Agent

--- nix/k8s/timescale.nix
+++ nix/k8s/timescale.nix
@@ -1,3 +1,16 @@
 { lib }:
 let name = "timescale"; image = "ghcr.io/opendesk-edu/timescale"; tag = "latest";
-in [ (lib.statefulset { inherit name image tag; port = 5432; }) (lib.service { inherit name; port = 5432; }) ]
+in [
+  (lib.statefulset { 
+    inherit name image tag;
+    port = 5432;
+    volumeClaims = [
+      { name = "data"; spec = { 
+        accessModes = [ "ReadWriteOnce" ];
+        storageClassName = "ceph-rbd-ssd";
+        resources = { requests = { storage = "10Gi"; }; };
+      }; }
+    ];
+  })
+  (lib.service { inherit name; port = 5432; })
+]
